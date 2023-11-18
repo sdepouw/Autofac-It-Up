3cf47ccdbc101d74a7f2c1fd225887b3042e64c9
Scott DePouw <scott.depouw@gmail.com>
2023-11-18 11:45:07 -0500

    Basic tests of Autofac

---
+++ AutofacItUp.SomeLib.Tests/SomeLibAutofacModuleTests/Registrations.cs
@@ -0,0 +1,34 @@
+using Autofac;
+using FluentAssertions;
+
+namespace AutofacItUp.SomeLib.Tests.SomeLibAutofacModuleTests;
+
+public class Registrations
+{
+    [Fact]
+    public void RegistersSomeLibService()
+    {
+        IContainer autofacContainer = BuildContainerWithAutofacModule();
+
+        bool isRegistered = autofacContainer.IsRegistered<ISomeLibService>();
+
+        isRegistered.Should().BeTrue();
+    }
+
+    [Fact]
+    public void DoesNotRegisterServiceWhenPassedAsTypeToIgnore()
+    {
+        IContainer autofacContainer = BuildContainerWithAutofacModule(typeof(SomeLibService));
+
+        bool isRegistered = autofacContainer.IsRegistered<ISomeLibService>();
+
+        isRegistered.Should().BeFalse();
+    }
+
+    private static IContainer BuildContainerWithAutofacModule(params Type[] typesToIgnore)
+    {
+        var builder = new ContainerBuilder();
+        builder.RegisterModule(new SomeLibAutofacModule(typesToIgnore));
+        return builder.Build();
+    }
+}
