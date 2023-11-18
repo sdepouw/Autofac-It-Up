6b4d08567d94a873fb2bf5e470376c647b599e52
Scott DePouw <scott.depouw@gmail.com>
2023-11-18 11:17:22 -0500

    Catching unfetched cat facts

--- AutofacItUp.Console/Worker.cs
+++ AutofacItUp.Console/Worker.cs
@@ -7,12 +7,25 @@ public class Worker(ILogger<Worker> logger, ISomeWorkerService someWorkerService
   protected override async Task ExecuteAsync(CancellationToken stoppingToken)
   {
     await Task.Delay(100, stoppingToken); // Wait for log messages
-    var facts = await client.GetTheFactsAsync(stoppingToken);
-    logger.LogInformation("[{Guid}] {FactsCount} fact(s)!", _guid, facts.Count);
+    await CatFactsTime(stoppingToken);
     while (!stoppingToken.IsCancellationRequested)
     {
       logger.LogInformation("[{Guid}] What's the time? {TimeTicks}", _guid, someWorkerService.GetTimeTicks());
       await Task.Delay(5000, stoppingToken);
+    }
+  }
+
+  private async Task CatFactsTime(CancellationToken stoppingToken)
+  {
+    try
+    {
+      logger.LogInformation("[{Guid}] Attempting to get cat facts... 🐈", _guid);
+      var facts = await client.GetTheFactsAsync(stoppingToken);
+      logger.LogInformation("[{Guid}] {FactsCount} fact(s)! 😽", _guid, facts.Count);
+    }
+    catch (Exception e)
+    {
+      logger.LogError(e, "[{Guid}] Could not get any cat facts. 😿", _guid);
     }
   }
 }
