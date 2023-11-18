8f372adcf4e47941caa888f7270b72d29511cc73
Scott DePouw <scott.depouw@gmail.com>
2023-11-18 08:50:11 -0500

    Add Serilog

--- AutofacItUp.Console/Program.cs
+++ AutofacItUp.Console/Program.cs
@@ -1,8 +1,10 @@
 using AutofacItUp.Console;
 using AutofacItUp.SomeLib;
+using Serilog;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.RegisterDependencies();
+builder.Services.AddSerilog(config => config.ReadFrom.Configuration(builder.Configuration));
 builder.Services.AddWindowsService();
 builder.Services.AddHostedService<Worker>();
 
